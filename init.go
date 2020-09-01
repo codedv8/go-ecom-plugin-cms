9c5c3b5844d4062589e62ed6bad25ad685455028
Thomas Björk <micoxdev@gmail.com>
2020-09-01 15:25:33 +0200

    Commit to ensure we are all on the same page

--- init.go
+++ init.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"context"
 	EComApp "github.com/codedv8/go-ecom-app"
 	EComStructs "github.com/codedv8/go-ecom-structs"
 	_ "github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
 )
+
+type Page struct {
+	Title  string `bson:"title"`
+	SEOURL string `bson:"seourl"`
+}
 
 func (cms *CMS) SysInit(app *EComApp.Application) {
 
@@ -15,8 +23,15 @@ func (cms *CMS) Init(app *EComApp.Application) {
 		switch c := payload.(type) {
 		case *EComStructs.RouterWildcard:
 			path := c.Context.Request.URL.Path
-			if len(path) == 10 {
-				c.Context.String(200, "Path was 10 chars in length. Plugin CMS responded to this call")
+			if path == "/" {
+				collection := app.DB.Client.Database("shop").Collection("cms")
+				var page bson.M
+				err := collection.FindOne(context.TODO(), bson.D{}).Decode(&page)
+				if err != nil {
+					log.Printf("Error FindOne %+v", err)
+					return false, err
+				}
+				c.Context.JSON(200, page)
 				return false, nil
 			}
 			return true, nil
